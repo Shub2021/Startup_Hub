0eda33106be2e14bff1c21f30761b6d9f459e42c
Ruwanthi Hemachandra <56576410+RSH17@users.noreply.github.com>
2021-08-26 20:33:57 +0530

    Update Jobs.js

--- screens/service/Jobs.js
+++ screens/service/Jobs.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   StyleSheet,
   SafeAreaView,
@@ -10,6 +11,7 @@ import {
   Alert,
   KeyboardAvoidingView,
   Button,
+  FlatList,
   Keyboard,
   Platform,
   TouchableOpacity,
@@ -22,6 +24,167 @@ import Urls from "../../constant";
 export default function Jobs(props) {
   //const data = props.route.params.data;
   const [selected, setSelected] = useState("0");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [br, setBr] = useState("");
+  const [cmpcategory, setCategory] = useState("");
+  const [data, setdata] = useState([]);
+  const [placed, setPlaced] = useState([]);
+  const [processing, setProcessing] = useState([]);
+  const [completed, setCompleted] = useState([]);
+  const [loading, setloading] = useState(true);
+  const getData = async () => {
+    const email = await AsyncStorage.getItem("email");
+    const name = await AsyncStorage.getItem("name");
+    const br = await AsyncStorage.getItem("br");
+    const cmp = await AsyncStorage.getItem("category");
+    fetch(Urls.cn + "/Jobs/" + br)
+      .then((res) => res.json())
+      .then((result) => {
+        //console.log(result);
+        setdata(result);
+      });
+    setEmail(email);
+    setBr(br);
+    setCategory(cmp);
+    setName(name);
+    setloading(false);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const startJob = (item, flag1) => {
+    props.navigation.navigate("JobTask", {item, flag1});
+   };
+ const showAlert1 = (item, flag1) =>
+     Alert.alert(
+       "Alert",
+         "Are you sure to start job?",
+       [
+         {
+           text: "Yes",
+           onPress: () => startJob(item, flag1),
+           style: "cancel",
+         },
+         {
+           text: "No",
+           style: "cancel",
+         },
+       ],
+       {
+         cancelable: true,
+       }
+     );
+  const renderList1 = (item) => {
+    const flag1 = true; 
+    const id = item._id.slice(18, 23);
+    if (item.job_status === "placed") {
+      return (
+        <TouchableOpacity
+          style={{
+            marginBottom: SIZES.radius,
+            borderRadius: SIZES.radius * 2,
+            paddingHorizontal: SIZES.padding,
+            paddingVertical: SIZES.radius,
+            backgroundColor: COLORS.gray3,
+          }}
+          onPress={() => showAlert1(item, flag1)}
+        >
+          <Text style={{ color: COLORS.lightYellow, fontSize: 24 }}>
+            Job #{id}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginRight: 5,
+            }}
+          >
+            <Text style={{ color: COLORS.white, fontSize: 18 }}>
+              {item.description}
+            </Text>
+            <Text style={{ color: COLORS.white, fontSize: 18 }}>
+              {item.job_status}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  };
+
+  const renderList2 = (item) => {
+    const flag1 = false;
+    const id = item._id.slice(18, 23);
+    if (item.job_status === "Processing") {
+      return (
+        <TouchableOpacity
+          style={{
+            marginBottom: SIZES.radius,
+            borderRadius: SIZES.radius * 2,
+            paddingHorizontal: SIZES.padding,
+            paddingVertical: SIZES.radius,
+            backgroundColor: COLORS.gray3,
+          }}
+          onPress={() => showAlert1(item, flag1)}
+        >
+          <Text style={{ color: COLORS.lightYellow, fontSize: 24 }}>
+            Job #{id}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginRight: 5,
+            }}
+          >
+            <Text style={{ color: COLORS.white, fontSize: 18 }}>
+              {item.description}
+            </Text>
+            <Text style={{ color: COLORS.white, fontSize: 18 }}>
+              {item.job_status}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  };
+
+  const renderList3 = (item) => {
+    const id = item._id.slice(18, 23);
+    if (item.job_status === "Completed") {
+      return (
+        <TouchableOpacity
+          style={{
+            marginBottom: SIZES.radius,
+            borderRadius: SIZES.radius * 2,
+            paddingHorizontal: SIZES.padding,
+            paddingVertical: SIZES.radius,
+            backgroundColor: COLORS.gray3,
+          }}
+        >
+          <Text style={{ color: COLORS.lightYellow, fontSize: 24 }}>
+            Job #{id}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginRight: 5,
+            }}
+          >
+            <Text style={{ color: COLORS.white, fontSize: 18 }}>
+              {item.description}
+            </Text>
+            <Text style={{ color: COLORS.white, fontSize: 18 }}>
+              {item.job_status}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  };
+
   return (
     <View
       behavior={Platform.OS === "ios" ? "padding" : "position"}
@@ -63,7 +226,7 @@ export default function Jobs(props) {
           </View>
         </View>
       </SafeAreaView>
-      <ScrollView style={styles.scrollcontainer}>
+      <View style={styles.scrollcontainer}>
         <View
           style={{
             flexDirection: "row",
@@ -143,7 +306,59 @@ export default function Jobs(props) {
             </TouchableOpacity>
           </View>
         </View>
-      </ScrollView>
+        {selected === "0" ? (
+              <View>
+                {/* Placed Orders */}
+                <FlatList
+                  style={{
+                    marginTop: SIZES.radius,
+                    paddingHorizontal: SIZES.radius,
+                  }}
+                  data={data}
+                  renderItem={({ item }) => {
+                    return renderList1(item);
+                  }}
+                  keyExtractor={(item) => item._id.toString()}
+                  onRefresh={() => getData()}
+                  refreshing={loading}
+                />
+              </View>
+            ) : selected === "1" ? (
+              <View>
+                {/* Processing Orders */}
+                <FlatList
+                  style={{
+                    marginTop: SIZES.radius,
+                    paddingHorizontal: SIZES.radius,
+                  }}
+                  data={data}
+                  renderItem={({ item }) => {
+                    return renderList2(item);
+                  }}
+                  keyExtractor={(item) => item._id.toString()}
+                  onRefresh={() => getData()}
+                  refreshing={loading}
+                />
+              </View>
+            ) : (
+              <View>
+                {/* Completed Orders */}
+                <FlatList
+                  style={{
+                    marginTop: SIZES.radius,
+                    paddingHorizontal: SIZES.radius,
+                  }}
+                  data={data}
+                  renderItem={({ item }) => {
+                    return renderList3(item);
+                  }}
+                  keyExtractor={(item) => item._id.toString()}
+                  onRefresh={() => getData()}
+                  refreshing={loading}
+                />
+              </View>
+            )}
+      </View>
     </View>
   );
 }
