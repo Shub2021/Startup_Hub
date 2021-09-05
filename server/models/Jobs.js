de7a533edab15366742a9155477720f3cef498f2
Ruwanthi Hemachandra <56576410+RSH17@users.noreply.github.com>
2021-09-05 17:39:15 +0530

    Update Jobs.js

--- server/models/Jobs.js
+++ server/models/Jobs.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
 const ToDoSchema = new mongoose.Schema({
-    id: Number,
-    task : String,
-    completed: Boolean,
-
-  });
+  id: Number,
+  task: String,
+  completed: Boolean,
+});
 
 const JobsSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   date: String,
   description: String,
   serviceid: String,
-  clientid: String,
+  client_email: String,
+  service_name: String,
+  package_id: String,
+  price: Number,
+  package_name: String,
   br_number: String,
   job_status: String,
   taskarray: [ToDoSchema],
